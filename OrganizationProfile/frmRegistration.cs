648f4bc9ea708160605e7b996884ba3e7ebc43e2
zronowa17-maker <zronowa17@gmail.com>
2025-09-06 22:14:42 +0800

    create ui design and add

--- OrganizationProfile/frmRegistration.cs
+++ OrganizationProfile/frmRegistration.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace OrganizationProfile
 {
-    public partial class Form1 : Form
+    public partial class frmRegistration : Form
     {
-        public Form1()
+        public frmRegistration()
         {
             InitializeComponent();
         }
