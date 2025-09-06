9cbf1f58520fe324837773d5f99bf23057b17030
zronowa17-maker <zronowa17@gmail.com>
2025-09-06 23:00:10 +0800

    add code

--- OrganizationProfile/frmRegistration.cs
+++ OrganizationProfile/frmRegistration.cs
@@ -23,7 +23,19 @@ namespace OrganizationProfile
 
         private void frmRegistration_Load(object sender, EventArgs e)
         {
-
+            string[] ListOfProgram = new string[]
+            {
+                 "BS Information Technology",
+                 "BS Computer Science",
+                 "BS Information Systems",
+                 "BS in Accountancy",
+                 "BS in Hospitality Management",
+                 "BS in Tourism Management"
+            };
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
         }
     }
 }
