ed3b65b8b27ce84b952d8037cef468f4c15ba95f
zronowa17-maker <zronowa17@gmail.com>
2025-09-06 22:27:20 +0800

    create class and add codes

---
+++ OrganizationProfile/StudentInformationClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    internal class StudentInformationClass
+    {
+        public static string SetStudentNumber = "";
+        public static string SetContactNo = "";
+        public static string SetProgram = "";
+        public static string SetGender = "";
+        public static string SetBirthday = "";
+        public static string SetFullName = "";
+        public static int SetAge = 0;
+    }
+
+}
